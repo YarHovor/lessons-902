1c23ecd14e9f14f14c254c2b7a5d59562cfff9c8
Yaroslav Hovorukha <yaroslav.hovorukha@gmail.com>
2019-03-05 21:01:00 +0200

    4й урок JS

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//koment
+/*
+alert('Hello world');
+console.log('Hello world');
+*/
+/*
+var message;
+var count;
+var messege1 = 'ffff', label, message3 = 'hhhhh';
+
+
+message = 'Text coobshenie';
+count = 2;
+
+alert(message);
+*/
+
+var num1, num2, result;
+
+num1 = +prompt('Введите 1ое число', 0);
+num2 = +prompt('Введите 2ое число', 0);
+result = num1 + num2;
+
+alert('Резултат чложениея: ' + result);
