4cfdce9cf694c3bb753f92c32bc7a5abd0747605
Yaroslav Hovorukha <yaroslav.hovorukha@gmail.com>
2019-04-05 18:28:44 +0300

    les 10

--- classes/Registry.php
+++ classes/Registry.php
@@ -1,32 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: skillup_student
- * Date: 02.04.19
- * Time: 19:30
- */
 
 class Registry
 {
-    /**
-     * @var Registry
-     */
-    private static $instance;
 
-    /**
-     * @var string
-     */
-    //private $formElementClass()
+	/**
+	 * @var Registry
+	 */
+	private static $instance;
 
-    private function __construct()
-    {
-    }
-    static public function getInstance()
-    {
-        if (!self::$instance){
-            self::$instance=new Registry();
-        }
-        return self::$instance;
-    }
-//  .......................V VIDOS
+	/**
+	 * @var string
+	 */
+	private $formElementClass;
+
+	private function __construct()
+	{
+	}
+
+	static public function getInstance()
+	{
+		if (!self::$instance) {
+			self::$instance = new Registry();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFormElementClass(): string
+	{
+		return $this->formElementClass;
+	}
+
+	/**
+	 * @param string $formElementClass
+	 */
+	public function setFormElementClass(string $formElementClass): void
+	{
+		$this->formElementClass = $formElementClass;
+	}
+
 }
